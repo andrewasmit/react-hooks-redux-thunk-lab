430978205ae37941e74c6f109b41e9088cd4d125
Andrew <andrewasmit@hotmail.com>
2023-05-08 13:53:07 -0500

    commit tree 1dffc38b3148424931449a4f8fbc31c314584ee4

--- src/features/cats/Cats.js
+++ src/features/cats/Cats.js
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchCats } from "./catsSlice";
 
 function Cats() {
+const catPics= useSelector(state=>state.entities);
+const dispatch= useDispatch();
+
+console.log(catPics)
+
+useEffect(()=>{
+  dispatch(fetchCats());
+}, []);
+
+
   return (
     <div>
       <h1>CatBook</h1>
--- src/features/cats/catsSlice.js
+++ src/features/cats/catsSlice.js
@@ -1,3 +1,34 @@
-export function fetchCats() {}
+export function fetchCats() {
+  return function (dispatch) {
+    dispatch({ type: "cats/catsLoading" });
+    fetch("https://learn-co-curriculum.github.io/cat-api/cats.json")
+      .then((res) => res.json())
+      .then((data) => {
+        // Instead of console.log-ing, use dispatch to update store.
+        dispatch({ type: "cats/catsLoaded", payload: data.images });
+      });
+  };
+}
 
-export default function catsReducer() {}
+const initialState = {
+  entities: [],
+  status: "idle",
+};
+
+export default function catsReducer(state = initialState, action) {
+  switch (action.type) {
+    case "cats/loading":
+      return {
+        ...state,
+        status: "loading",
+      };
+    case "cats/catsLoaded":
+      return {
+        ...state,
+        entities: action.payload,
+        status: "idle",
+      };
+    default:
+      return state;
+  }
+}
